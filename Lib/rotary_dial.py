b4a6fea6eb2f1524f22a7d575a21235d16ce1d04
robertmuth <robertmuth@github.com>
2019-08-04 17:39:24 -0400

    massive tweaking on dialer

--- Lib/rotary_dial.py
+++ Lib/rotary_dial.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
+import logging
+import time
+
 import RPi.GPIO as GPIO
+
+
+def CleanupTriggers(now, triggers):
+    print ("")
+    for t in triggers:
+        delta = now - t[0]
+        if delta < 0.05: continue
+        print (delta, t[1])
 
 class RotaryDial(object):
 
@@ -8,28 +19,39 @@ class RotaryDial(object):
         self._pin_lifted = pin_lifted
         self._pin_trigger = pin_trigger
         self._cb_dialed = cb_dialed
+        self._idle = True
         GPIO.setup(pin_lifted, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(pin_trigger, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.add_event_detect(pin_lifted, GPIO.BOTH, callback=self._lifted, bouncetime=200)
-        GPIO.add_event_detect(pin_trigger, GPIO.BOTH, callback=self._trigger, bouncetime=80)
-        self._count = 0
+        # do not change these lightly - these were carefully tweaked
+        GPIO.add_event_detect(pin_lifted, GPIO.BOTH, callback=self._lifted, bouncetime=25)
+        GPIO.add_event_detect(pin_trigger, GPIO.RISING, callback=self._trigger, bouncetime=10)
+        self._triggers = []
         
     def _lifted(self, pin):
-        if self._count > 0:
-            self._cb_dialed(self._count)
-            self._count = 0
+        now = time.time()
+        idle =  GPIO.input(pin)
+        if idle == self._idle:
+            return
+        self._idle = idle
+        logging.info("idle: %d", idle)
+        # we just became idle
+        if self._idle:
+            self._cb_dialed(len(self._triggers))
         else:
             self._cb_dialed(0)
+        self._triggers = []
+
 
     def _trigger(self, pin):
-        lifted = GPIO.input(self._pin_lifted) == GPIO.LOW
-        if lifted:
-            self._count += 1
-        self._cb_dialed(-1)            
-        
+        ts = time.time()
+        if not self._idle:
+            v  = GPIO.input(pin)
+            if v:
+                self._triggers.append((ts, v))
         
 if __name__ == "__main__":
-    import time
+
+    logging.basicConfig(level=logging.INFO)
     GPIO.setmode(GPIO.BCM)
     def dialed(n):
         print ("dialed", n)
